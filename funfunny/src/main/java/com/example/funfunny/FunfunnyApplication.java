3f323177929dfb193a9eca5f8776eafa22917195
komin86 <komin86@naver.com>
2019-12-09 14:59:01 +0900

    Add files via upload

---
+++ funfunny/src/main/java/com/example/funfunny/FunfunnyApplication.java
@@ -0,0 +1,13 @@
+package com.example.funfunny;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FunfunnyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FunfunnyApplication.class, args);
+	}
+
+}
---
+++ funfunny/src/test/java/com/example/funfunny/FunfunnyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.funfunny;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FunfunnyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
