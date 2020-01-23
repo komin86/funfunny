297b6eb9148099d1759579a2938a924b1469ab2b
komin <komin@DESKTOP-FENH1E1>
2020-01-23 11:07:09 +0900

    작업중

---
+++ funfunny/src/main/resources/static/js/common/common.js
@@ -0,0 +1,4 @@
+function hideAndShow(hideEl , showEl){
+	document.querySelector(hideEl).style.setProperty("display","none");
+	document.querySelector(showEl).style.setProperty("display","block");
+}
